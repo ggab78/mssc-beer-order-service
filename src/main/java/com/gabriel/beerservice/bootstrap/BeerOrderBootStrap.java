9ecbdf7d03d2ffb865454760a94ee6e51c98d7d6
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2020-10-23 17:54:27 +0200

    bootstrapdata

--- src/main/java/com/gabriel/beerservice/bootstrap/BeerOrderBootStrap.java
+++ src/main/java/com/gabriel/beerservice/bootstrap/BeerOrderBootStrap.java
@@ -16,9 +16,6 @@ import java.util.UUID;
 public class BeerOrderBootStrap implements CommandLineRunner {
 
     public static final String TASTING_ROOM = "Tasting Room";
-    public static final String BEER_1_UPC = "0631234200036";
-    public static final String BEER_2_UPC = "0631234300019";
-    public static final String BEER_3_UPC = "0083783375213";
 
     private final CustomerRepository customerRepository;
 
